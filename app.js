8d1bef2c755d5f49df4dad14605c2413cc55486e
marcelo bonda <marcelo.pf16@gmail.com>
2023-10-21 16:40:44 -0300

    productManager actualizado

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const {ProductManager,productManager} = require('../desafio2/ProductManager'); 
+const {productManager} = require('../desafio2/ProductManager'); 
 
-productManager.addProduct('producto de prueba','esto es un producto de prueba',200,'sin imagen','abc123',25)
 
-console.log(productManager.getProducts())
 
 
 const app = express();
@@ -14,10 +12,10 @@ const port = 8080;
 
 app.use(express.json());
 
-// Endpoint para obtener todos los productos con opción de límite
-app.get('/products', async (req, res) => {
+
+app.get('/products',  (req, res) => {
 const { limit } = req.query;
-const products = await productManager.getProducts();
+const products =  productManager.getProducts();
 
 if (limit) {
 const limitedProducts = products.slice(0, parseInt(limit));
@@ -27,8 +25,7 @@ res.json(products);
 }
 });
 
-// Endpoint para obtener un producto por su ID
-app.get('/products/:pid', async (req, res) => {
+app.get('/products/:productId', async (req, res) => {
 const { pid } = req.params;
 const products = await productManager.getProducts();
 const product = products.find(p => p.id === parseInt(pid));
@@ -40,9 +37,7 @@ res.status(404).json({ error: 'Producto no encontrado' });
 }
 });
 
-app.get('/bienvenida', (req,res)=>{
-    res.send('esto funciona')
-})
+
 
 app.listen(port, () => {
 console.log(`Servidor Express corriendo en el puerto ${port}`);
